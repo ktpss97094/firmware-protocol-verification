4222caaf6e6577f1a2fccefc4ddeaebec3b3a6e2
DavidPeng <ktpss97094@gapp.nthu.edu.tw>
2026-03-09 22:28:58 +0800

    feat: successfully verify TWI master receiver blocking mode

--- src/project/peripherals/nrf52840/twi.py
+++ src/project/peripherals/nrf52840/twi.py
@@ -5,16 +5,26 @@ from project.types import MMIOMemoryRegion
 
 
 class TWI(MMIOMemoryRegion):
+    class TASKS_STARTRX:
+        OFFSET = 0x000
+
     class TASKS_STARTTX:
         OFFSET = 0x008
 
     class TASKS_STOP:
         OFFSET = 0x014
 
+        TASKS_STOP = 0
+
     class EVENTS_STOPPED:
         OFFSET = 0x104
 
         EVENTS_STOPPED = 0
+
+    class EVENTS_RXDREADY:
+        OFFSET = 0x108
+
+        EVENTS_RXDREADY = 0
 
     class EVENTS_TXDSENT:
         OFFSET = 0x11C
@@ -36,6 +46,9 @@ class TWI(MMIOMemoryRegion):
         BB = 14
         SUSPENDED = 18
 
+    class RXD:
+        OFFSET = 0x518
+
     class TXD:
         OFFSET = 0x51C
 
@@ -45,14 +58,55 @@ class TWI(MMIOMemoryRegion):
 
         self.pre_read(state, offset)
 
-        events_txdsent = utils.load(state, self.start + TWI.EVENTS_TXDSENT.OFFSET)
+        tasks_stop = utils.load(state, self.start + TWI.TASKS_STOP.OFFSET)
         events_stopped = utils.load(state, self.start + TWI.EVENTS_STOPPED.OFFSET)
+        events_rxdready = utils.load(state, self.start + TWI.EVENTS_RXDREADY.OFFSET)
+        events_txdsent = utils.load(state, self.start + TWI.EVENTS_TXDSENT.OFFSET)
         events_error = utils.load(state, self.start + TWI.EVENTS_ERROR.OFFSET)
-        new_events_txdsent = events_txdsent
+        rxd = utils.load(state, self.start + TWI.RXD.OFFSET)
         new_events_stopped = events_stopped
+        new_events_rxdready = events_rxdready
+        new_events_txdsent = events_txdsent
         new_events_error = events_error
+        new_rxd = rxd
 
         match offset:
+            case TWI.EVENTS_STOPPED.OFFSET:
+                if events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED].symbolic:
+                    new_events_stopped = utils.replace_bit(
+                        new_events_stopped,
+                        TWI.EVENTS_STOPPED.EVENTS_STOPPED,
+                        claripy.If(
+                            events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED] == 1,
+                            events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED],
+                            utils.generate_symbolic(
+                                state,
+                                f"{self.name}_{TWI.EVENTS_STOPPED.OFFSET:#x}_EVENTS_STOPPED",
+                                size=1,
+                            ),
+                        ),
+                    )
+
+            case TWI.EVENTS_RXDREADY.OFFSET:
+                if events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY].symbolic:
+                    new_events_rxdready = utils.replace_bit(
+                        new_events_rxdready,
+                        TWI.EVENTS_RXDREADY.EVENTS_RXDREADY,
+                        claripy.If(
+                            events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY] == 1,
+                            events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY],
+                            utils.generate_symbolic(
+                                state,
+                                f"{self.name}_{TWI.EVENTS_RXDREADY.OFFSET:#x}_EVENTS_RXDREADY",
+                                size=1,
+                            ),
+                        ),
+                    )
+                    state.globals[f"{self.name}_EVENTS_RXDREADY_set"] = claripy.Or(
+                        state.globals.get(f"{self.name}_EVENTS_RXDREADY_set", False),
+                        new_events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY] == 1,
+                    )
+
             case TWI.EVENTS_TXDSENT.OFFSET:
                 if events_txdsent[TWI.EVENTS_TXDSENT.EVENTS_TXDSENT].symbolic:
                     new_events_txdsent = utils.replace_bit(
@@ -71,22 +125,6 @@ class TWI(MMIOMemoryRegion):
                     state.globals[f"{self.name}_EVENTS_TXDSENT_set"] = claripy.Or(
                         state.globals.get(f"{self.name}_EVENTS_TXDSENT_set", False),
                         new_events_txdsent[TWI.EVENTS_TXDSENT.EVENTS_TXDSENT] == 1,
-                    )
-
-            case TWI.EVENTS_STOPPED.OFFSET:
-                if events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED].symbolic:
-                    new_events_stopped = utils.replace_bit(
-                        new_events_stopped,
-                        TWI.EVENTS_STOPPED.EVENTS_STOPPED,
-                        claripy.If(
-                            events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED] == 1,
-                            events_stopped[TWI.EVENTS_STOPPED.EVENTS_STOPPED],
-                            utils.generate_symbolic(
-                                state,
-                                f"{self.name}_{TWI.EVENTS_STOPPED.OFFSET:#x}_EVENTS_STOPPED",
-                                size=1,
-                            ),
-                        ),
                     )
 
             case TWI.EVENTS_ERROR.OFFSET:
@@ -109,9 +147,37 @@ class TWI(MMIOMemoryRegion):
                         new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1,
                     )
 
-        utils.store(state, self.start + TWI.EVENTS_TXDSENT.OFFSET, new_events_txdsent)
+            case TWI.RXD.OFFSET:
+                # 在 set TASKS_STOP 後並 read RXD，會送出 NACK 表示 read 結束
+                if state.solver.is_true(tasks_stop[TWI.TASKS_STOP.TASKS_STOP] != 1):
+                    new_events_rxdready = utils.symbolic_bit(
+                        state,
+                        events_rxdready,
+                        TWI.EVENTS_RXDREADY.EVENTS_RXDREADY,
+                        f"{self.name}_{TWI.EVENTS_RXDREADY.OFFSET:#x}_EVENTS_RXDREADY",
+                    )
+                    state.globals[f"{self.name}_EVENTS_RXDREADY_set"] = (
+                        new_events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY] == 1
+                    )
+
+                    new_events_error = utils.symbolic_bit(
+                        state,
+                        events_error,
+                        TWI.EVENTS_ERROR.EVENTS_ERROR,
+                        f"{self.name}_{TWI.EVENTS_ERROR.OFFSET:#x}_EVENTS_ERROR",
+                    )
+                    state.globals[f"{self.name}_EVENTS_ERROR_set"] = (
+                        new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1
+                    )
+
+                # Product Specification: Register is cleared on read
+                new_rxd = claripy.BVV(0, state.arch.bits)
+
         utils.store(state, self.start + TWI.EVENTS_STOPPED.OFFSET, new_events_stopped)
+        utils.store(state, self.start + TWI.EVENTS_RXDREADY.OFFSET, new_events_rxdready)
+        utils.store(state, self.start + TWI.EVENTS_TXDSENT.OFFSET, new_events_txdsent)
         utils.store(state, self.start + TWI.EVENTS_ERROR.OFFSET, new_events_error)
+        utils.store(state, self.start + TWI.RXD.OFFSET, new_rxd)
 
     def write(self, state):
         addr = state.solver.eval(state.inspect.mem_write_address)
@@ -120,14 +186,59 @@ class TWI(MMIOMemoryRegion):
 
         self.pre_write(state, offset, value)
 
-        events_txdsent = utils.load(state, self.start + TWI.EVENTS_TXDSENT.OFFSET)
         events_stopped = utils.load(state, self.start + TWI.EVENTS_STOPPED.OFFSET)
+        events_rxdready = utils.load(state, self.start + TWI.EVENTS_RXDREADY.OFFSET)
+        events_txdsent = utils.load(state, self.start + TWI.EVENTS_TXDSENT.OFFSET)
         events_error = utils.load(state, self.start + TWI.EVENTS_ERROR.OFFSET)
-        new_events_txdsent = events_txdsent
         new_events_stopped = events_stopped
+        new_events_rxdready = events_rxdready
+        new_events_txdsent = events_txdsent
         new_events_error = events_error
 
         match offset:
+            case TWI.TASKS_STARTRX.OFFSET:
+                if state.solver.is_true(value == 1):
+                    new_events_rxdready = utils.symbolic_bit(
+                        state,
+                        events_rxdready,
+                        TWI.EVENTS_RXDREADY.EVENTS_RXDREADY,
+                        f"{self.name}_{TWI.EVENTS_RXDREADY.OFFSET:#x}_EVENTS_RXDREADY",
+                    )
+                    state.globals[f"{self.name}_EVENTS_RXDREADY_set"] = (
+                        new_events_rxdready[TWI.EVENTS_RXDREADY.EVENTS_RXDREADY] == 1
+                    )
+
+                    new_events_error = utils.symbolic_bit(
+                        state,
+                        events_error,
+                        TWI.EVENTS_ERROR.EVENTS_ERROR,
+                        f"{self.name}_{TWI.EVENTS_ERROR.OFFSET:#x}_EVENTS_ERROR",
+                    )
+                    state.globals[f"{self.name}_EVENTS_ERROR_set"] = (
+                        new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1
+                    )
+
+            case TWI.TASKS_STARTTX.OFFSET:
+                if state.solver.is_true(value == 1):
+                    new_events_error = utils.symbolic_bit(
+                        state,
+                        events_error,
+                        TWI.EVENTS_ERROR.EVENTS_ERROR,
+                        f"{self.name}_{TWI.EVENTS_ERROR.OFFSET:#x}_EVENTS_ERROR",
+                    )
+                    state.globals[f"{self.name}_EVENTS_ERROR_set"] = (
+                        new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1
+                    )
+
+            case TWI.TASKS_STOP.OFFSET:
+                if state.solver.is_true(value == 1):
+                    new_events_stopped = utils.symbolic_bit(
+                        state,
+                        events_stopped,
+                        TWI.EVENTS_STOPPED.EVENTS_STOPPED,
+                        f"{self.name}_{TWI.EVENTS_STOPPED.OFFSET:#x}_EVENTS_STOPPED",
+                    )
+
             case TWI.TXD.OFFSET:
                 new_events_txdsent = utils.symbolic_bit(
                     state,
@@ -150,27 +261,7 @@ class TWI(MMIOMemoryRegion):
                     new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1
                 )
 
-            case TWI.TASKS_STOP.OFFSET:
-                if state.solver.is_true(value == 1):
-                    new_events_stopped = utils.symbolic_bit(
-                        state,
-                        events_stopped,
-                        TWI.EVENTS_STOPPED.EVENTS_STOPPED,
-                        f"{self.name}_{TWI.EVENTS_STOPPED.OFFSET:#x}_EVENTS_STOPPED",
-                    )
-
-            case TWI.TASKS_STARTTX.OFFSET:
-                if state.solver.is_true(value == 1):
-                    new_events_error = utils.symbolic_bit(
-                        state,
-                        events_error,
-                        TWI.EVENTS_ERROR.EVENTS_ERROR,
-                        f"{self.name}_{TWI.EVENTS_ERROR.OFFSET:#x}_EVENTS_ERROR",
-                    )
-                    state.globals[f"{self.name}_EVENTS_ERROR_set"] = (
-                        new_events_error[TWI.EVENTS_ERROR.EVENTS_ERROR] == 1
-                    )
-
-        utils.store(state, self.start + TWI.EVENTS_TXDSENT.OFFSET, new_events_txdsent)
         utils.store(state, self.start + TWI.EVENTS_STOPPED.OFFSET, new_events_stopped)
+        utils.store(state, self.start + TWI.EVENTS_RXDREADY.OFFSET, new_events_rxdready)
+        utils.store(state, self.start + TWI.EVENTS_TXDSENT.OFFSET, new_events_txdsent)
         utils.store(state, self.start + TWI.EVENTS_ERROR.OFFSET, new_events_error)
