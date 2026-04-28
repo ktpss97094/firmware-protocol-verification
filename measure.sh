18ad480e53d6745f2ae2b925d19dfc0c9dc63543
DavidPeng <ktpss97094@gapp.nthu.edu.tw>
2026-04-28 19:53:55 +0800

    修改 measure.sh，增加更多優化

--- measure.sh
+++ measure.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "Use sudo to run this script"
+    exit 1
+fi
+
 if [ $# -eq 0 ]; then
     echo "Usage: $0 <command>"
     exit 1
 fi
 
+cleanup() {
+    sysctl -w kernel.randomize_va_space=2 > /dev/null
+}
+
+trap cleanup EXIT
+
 TIME_OUT=$(mktemp)
+
+sysctl -w kernel.randomize_va_space=0 > /dev/null
+sync; echo 3 > /proc/sys/vm/drop_caches
 
 /usr/bin/time -f "%e %M %P %U %S" -o "$TIME_OUT" "$@"
 
 STATS=$(awk '$1 ~ /^[0-9.]+$/ && NF >= 5 {print $0; exit}' "$TIME_OUT")
-
 read -r ELAPSED_SEC MAX_RSS_KB CPU_PER USER_SEC SYS_SEC <<< "$STATS"
 rm "$TIME_OUT"
 
