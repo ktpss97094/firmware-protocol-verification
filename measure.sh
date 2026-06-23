c38fe6112bd7987bd7d3dc365fdf0a8363449d5c
DavidPeng <ktpss97094@gapp.nthu.edu.tw>
2026-06-23 20:29:29 +0800

    更新 meashre.sh

--- measure.sh
+++ measure.sh
@@ -22,10 +22,26 @@ sysctl -w kernel.randomize_va_space=0 > /dev/null
 sync; echo 3 > /proc/sys/vm/drop_caches
 
 /usr/bin/time -f "%e %M %P %U %S" -o "$TIME_OUT" "$@"
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -ne 0 ]; then
+    rm -f "$TIME_OUT"
+    
+    if [ $EXIT_CODE -gt 128 ]; then
+        SIGNAL=$((EXIT_CODE - 128))
+       
+        trap - EXIT
+        cleanup
+        kill -$SIGNAL $$
+    else
+        exit $EXIT_CODE
+    fi
+fi
 
 STATS=$(awk '$1 ~ /^[0-9.]+$/ && NF >= 5 {print $0; exit}' "$TIME_OUT")
-read -r ELAPSED_SEC MAX_RSS_KB CPU_PER USER_SEC SYS_SEC <<< "$STATS"
 rm "$TIME_OUT"
+
+read -r ELAPSED_SEC MAX_RSS_KB CPU_PER USER_SEC SYS_SEC <<< "$STATS"
 
 ELAPSED_SEC=$(awk "BEGIN {print int($ELAPSED_SEC)}")
 MAX_RSS_MB=$(awk "BEGIN {print int($MAX_RSS_KB / 1024)}")
